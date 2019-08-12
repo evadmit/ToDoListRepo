8a718fdee1962cac55fae70949104e6c55945a24
devf5030 <devf5030@gmail.com>
2019-08-12 16:18:00 +0300

    todo controller implemented

--- backend/to-do-server/src/todo/todo.controller.ts
+++ backend/to-do-server/src/todo/todo.controller.ts
@@ -1,13 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus, Post, Body, Delete, Query, NotFoundException } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { TodoDto } from './models/todo.dto';
+import { ValidateObjectId } from 'src/shared/pipes/validate-object-id.pipes';
+
 
 @Controller('todo')
 export class TodoController {
-    @Get()
-    getTodos(){
-      return {
-          todo: 'hey, I`m working!'
-      }
-    }
 
+constructor(private readonly toDoService:TodoService) {}
 
+@Get('todos')
+async findAll(@Res() res) {
+  const todos= await  this.toDoService.findAll();
+ return res.status(HttpStatus.OK).json(todos);
+ }
+ 
+ @Post('add-todo')
+ async create(@Res() res, @Body() todoDto:TodoDto) {
+
+  const newTodo= await this.toDoService.create(todoDto);
+ return res.status(HttpStatus.OK).json({message:"Item added", todo: newTodo})
+  }
+
+  @Delete('delete')
+  async deleteTodo(@Res() res,@Query('todoID', new ValidateObjectId())todoID){
+    const deletedTodoItem  =await this.toDoService.deleteTodo(todoID);
+
+    if(!deletedTodoItem) throw new NotFoundException('Item doesn`t exist');
+    return res.status(HttpStatus.OK).json({
+      message: 'Item deleted',
+      todo: deletedTodoItem
+    })
+  }
+
+  @Post('edit')
+  async editTodo(@Res() res, @Query('todoID',new ValidateObjectId())todoID, @Body() todoDto:TodoDto){
+
+    const editedTodo= await this.toDoService.editTodo(todoID, todoDto);
+    if(!editedTodo) throw new NotFoundException ('Item doesn`t extst');
+
+    return res.status(HttpStatus.OK).json({
+      message:'Item updated',
+      todo: editedTodo
+    })
+
+  }
 }
