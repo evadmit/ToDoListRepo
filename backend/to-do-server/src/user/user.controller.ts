8c329bc111c6c56187980ea5e8595a7dd48fa66b
devf5030 <devf5030@gmail.com>
2019-08-12 16:25:53 +0300

    user controller implemented

--- backend/to-do-server/src/user/user.controller.ts
+++ backend/to-do-server/src/user/user.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
+import { UserDto } from './models/user.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
 export class UserController {
 
-    @Get()
-    getUser(){
-      return {
-          user: 'It`s me'
-      }
-}
+    constructor(private readonly userService:UserService) {}
+
+    @Get('login')
+    @UseGuards(AuthGuard())
+    async loginUser(): Promise<User>{
+        return this.userService.find(2);
+    }
+
+    @Post('register')
+    async create(@Body() userDto: UserDto){
+        this.userService.create(userDto);
+    }
+
+    @Get('google-login')
+    @UseGuards(AuthGuard())
+    async userGoogleLogin(): Promise<User[]>{
+        return this.userService.findAll();
+    }
+
+
 }
--- backend/to-do-server/src/user/user.module.ts
+++ backend/to-do-server/src/user/user.module.ts
@@ -3,9 +3,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './user.schema';
-
+import { PassportModule } from '@nestjs/passport';
+const passportModule = PassportModule.register({ defaultStrategy: 'jwt' });
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'bearer' }),
     MongooseModule.forFeature([
       { name: 'User', schema: UserSchema },
     ]),
